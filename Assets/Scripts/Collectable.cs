4e16615d66198ff819800cb91938f02de48402b7
LaureBedu6 <bedu.laure@wanadoo.fr>
2018-11-21 20:00:34 -0600

    Autres ressources + txt flottants qd collecte

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -15,6 +15,8 @@ public class Collectable : MonoBehaviour {
     private Quaternion targetRotation;
     private PlayerController playerScript;
 
+    private GameObject floatTextPrefab;
+
 
     [SerializeField] private bool isEmpty = false;
 
@@ -24,33 +26,34 @@ public class Collectable : MonoBehaviour {
         initialPosition = this.transform.position;
         initialRotation = transform.rotation;
 
+        floatTextPrefab = GameObject.FindGameObjectWithTag("floatTxt");
 
 
         switch (collectableType) {
             case "BigGoldRock":
-                ressourcePts = 250;
-                maxRessourcePts = 250;
-                durationRepop = 160f;
+                ressourcePts = 200;
+                maxRessourcePts = 200;
+                durationRepop = 100f;
                 break;
             case "BigRock":
                 ressourcePts = 100;
                 maxRessourcePts = 100;
-                durationRepop = 180f;
+                durationRepop = 120f;
                 break;
             case "SmallRock":
                 ressourcePts = 20;
                 maxRessourcePts = 20;
-                durationRepop = 120f;
+                durationRepop = 80f;
                 break;
             case "BigTree":
                 ressourcePts = 120;
                 maxRessourcePts = 150;
-                durationRepop = 130f;
+                durationRepop = 90f;
                 break;
             case "SmallTree":
                 ressourcePts = 40;
                 maxRessourcePts = 40;
-                durationRepop = 125f;
+                durationRepop = 75f;
                 break;
             default:
                 break;
@@ -60,7 +63,7 @@ public class Collectable : MonoBehaviour {
 	// Update is called once per frame
 	public void PickRessources() {
 
-        if (ressourcePts > 0 && !isEmpty)
+        if (ressourcePts > 1 && !isEmpty)
         {
             switch (collectableType)
             {
@@ -68,28 +71,33 @@ public class Collectable : MonoBehaviour {
                     ressourcePts -= 50;
                     this.transform.position += new Vector3(0, (float)-0.5, 0);
                     playerScript.setGold(playerScript.getGold() + 50);
+                    showFloatTxtRessources("+50 G", new Color32(255, 254, 103,255));
                     break;
                 case "BigRock":
                     ressourcePts -= 25;
                     this.transform.position += new Vector3(0, (float)-0.5, 0);
                     playerScript.setStone(playerScript.getStone() + 25);
+                    showFloatTxtRessources("+25 S", new Color32(125, 126, 128, 255));
                     break;
                 case "SmallRock":
                     ressourcePts -= 5;
                     this.transform.position += new Vector3(0, (float)-0.5, 0);
                     playerScript.setStone(playerScript.getStone() + 5);
+                    showFloatTxtRessources("+5 S", new Color32(125, 126, 128, 255));
                     break;
                 case "BigTree":
                     ressourcePts -= 30;
                     targetRotation = Quaternion.FromToRotation(initialRotation.eulerAngles, new Vector3(90,0,0));
                     //if (ressourcePts <= maxRessourcePts - 30) transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10);
                     playerScript.setWood(playerScript.getWood() + 30);
+                    showFloatTxtRessources("+30 W",  new Color32(232, 185, 151,255));
                     break;
                 case "SmallTree":
                     ressourcePts -= 10;
                     targetRotation = Quaternion.FromToRotation(initialRotation.eulerAngles, new Vector3(90, 0, 0));
                     //if (ressourcePts <= maxRessourcePts - 10) transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10);
                     playerScript.setWood(playerScript.getWood() + 10);
+                    showFloatTxtRessources("+10 W", new Color32(232, 185, 151,255));
                     break;
             }
         }
@@ -127,11 +135,9 @@ public class Collectable : MonoBehaviour {
                 break;
             case "BigTree":
             case "SmallTree":
-                if (isEmpty && this.transform.rotation != initialRotation)
+                if (isEmpty)
                 {
                     if (ressourcePts <= maxRessourcePts) ressourcePts += (float)0.025;
-                    if (ressourcePts >= maxRessourcePts) transform.rotation = Quaternion.Slerp(transform.rotation, initialRotation, (float)0.1);
-                    Debug.Log(this.transform.rotation == initialRotation);
                 }
 
                 else isEmpty = false;
@@ -140,6 +146,15 @@ public class Collectable : MonoBehaviour {
         }
 
     }
+
+    private void showFloatTxtRessources(string txt, Color color)
+    {
+        GameObject newTxtFloat = Instantiate(floatTextPrefab, floatTextPrefab.transform.position + new Vector3(0, 150,0), Quaternion.identity);
+        newTxtFloat.SetActive(true);
+        newTxtFloat.GetComponent<FloatTextController>().SetTextandMove(txt, color);
+        Destroy(newTxtFloat.gameObject, (float)0.6);
+    }
+
 
     public bool getIsEmpty()
     {
