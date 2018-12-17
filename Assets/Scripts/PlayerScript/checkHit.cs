9cd26bab2dd0497891d4b68652518b9d36a7690c
LaureBedu6 <43894723+LaureBedu6@users.noreply.github.com>
2018-12-16 18:41:20 -0600

    Update checkHit.cs

--- Assets/Scripts/PlayerScript/checkHit.cs
+++ Assets/Scripts/PlayerScript/checkHit.cs
@@ -1,17 +1,31 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class checkHit : MonoBehaviour {
 
 	[SerializeField] Animator player;
+    private GameObject floatTextPrefab;
 
-	void OnTriggerEnter(Collider other) {
+    void Start()
+    {
+        floatTextPrefab = GameObject.FindGameObjectWithTag("floatTxt");
+    }
+    void OnTriggerEnter(Collider other) {
 		if (other.gameObject.GetComponent<Monster>() && player.GetBool("isAttacking") && !player.GetComponent<PlayerController>().hasAttacked) {
 			Debug.Log(other.tag);
 			player.GetComponent<PlayerController>().hasAttacked = true;
 			Monster carac = other.gameObject.GetComponent<Monster>();
 			carac.setHealth(carac.getHealth() - player.GetComponent<PlayerController>().ATK);
-		}
+            showFloatTxtRessources("-"+player.GetComponent<PlayerController>().ATK, new Color32(205, 5, 5, 255));
+        }
 	}
+
+    private void showFloatTxtRessources(string txt, Color color)
+    {
+        GameObject newTxtFloat = Instantiate(floatTextPrefab, floatTextPrefab.transform.position + new Vector3(0, 150, 0), Quaternion.identity);
+        newTxtFloat.SetActive(true);
+        newTxtFloat.GetComponent<FloatTextController>().SetTextandMove(txt, color);
+        Destroy(newTxtFloat.gameObject, (float)0.6);
+    }
 }
